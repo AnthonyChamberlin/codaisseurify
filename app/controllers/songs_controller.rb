e38e55689a60dcb763d5cf5688b5c41b37d02424
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-12 23:48:01 +0200

    Add New Song to Artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,20 @@
 class SongsController < ApplicationController
-  before_action :find_song
+  before_action :find_song, only: [:show, :edit, :update]
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+
+    @song = Song.new(song_params)
+
+    if @song.save
+       redirect_to root_path
+    else
+       render 'new'
+    end
+  end
 
   def show; end
 
@@ -7,5 +22,9 @@ class SongsController < ApplicationController
 
   def find_song
     @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id)
   end
 end
