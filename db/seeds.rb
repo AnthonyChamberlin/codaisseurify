782a653c51712f8952b68791108423be5d314339
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-12 19:56:25 +0200

    Create Artist Model and Add Seeds for Artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.destroy_all
 Song.destroy_all
+
+artist1 = Artist.create!({ first_name: "Stevie", last_name: "Wonder"})
+artist2 = Artist.create!({ first_name: "B.B.", last_name: "King"})
+artist3 = Artist.create!({ first_name: "John Lee", last_name: "Hooker"})
+artist4 = Artist.create!({ first_name: "John", last_name: "Mayer"})
+
 
 # WILL BE MOTOWN SONGS
 song1 = Song.create!({ name: "Superstition" })
