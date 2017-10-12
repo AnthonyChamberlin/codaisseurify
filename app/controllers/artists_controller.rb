6ea7cee9dcac0de63ee19cea46035bffd03a4be0
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-12 21:35:42 +0200

    Add link to Artists Name in Index to redirect to Artist Show Page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,15 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :edit, :update]
+
   def index
     @artists = Artist.all.order("created_at DESC")
+  end
+
+  def show; end
+
+  private
+
+  def find_artist
+    @artist = Artist.find(params[:id])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
   has_many :songs
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
